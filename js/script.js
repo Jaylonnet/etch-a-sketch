45cb437bae3040d3061a526acdeb51f5be44883f
Jaylon <jaylonwingfield10@gmail.com>
2022-03-29 22:34:36 +0100

    Added click events to get rgb value of colour

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
 const boxes = document.querySelectorAll('.box');
-
-const re = /[0-9].[0-9][0-9]/
-
+const colours = document.querySelectorAll('#colours div');
+let currentColour = null;
+const rgbAlpha = /[0-9].[0-9][0-9]/
 // we use the .forEach method to iterate through each button
 boxes.forEach((box) => {
-
-  // and for each one we add a 'click' listener
   box.addEventListener('mouseover', () => {
     let rgba = window.getComputedStyle(box).getPropertyValue('background-color');
-    let current_rgba = parseFloat(rgba.match(re));
+    let current_rgba = parseFloat(rgba.match(rgbAlpha));
     if (current_rgba < 1.0) {
     new_rgba = current_rgba + 0.1
     box.style.backgroundColor = `rgba(0, 0, 0, ${new_rgba})`
     console.log(new_rgba)
     };
+  });
+});
+
+colours.forEach((colour) => {
+  colour.addEventListener('click', () => {
+    currentColour = window.getComputedStyle(colour).getPropertyValue('background-color');
+    console.log(currentColour)
   });
 });
