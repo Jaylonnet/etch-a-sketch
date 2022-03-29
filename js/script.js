76208f6fa21e39bd4050111d3f20a8620d14db8a
Jaylon <jaylonwingfield10@gmail.com>
2022-03-29 23:12:00 +0100

    Used replace method to get rgb values only

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const boxes = document.querySelectorAll('.box');
 const colours = document.querySelectorAll('#colours div');
-let currentColour = null;
+let currentColour = '236, 240, 241';
 const rgbAlpha = /[0-9].[0-9][0-9]/
 // we use the .forEach method to iterate through each button
 boxes.forEach((box) => {
@@ -10,7 +10,6 @@ boxes.forEach((box) => {
     if (current_rgba < 1.0) {
     new_rgba = current_rgba + 0.1
     box.style.backgroundColor = `rgba(0, 0, 0, ${new_rgba})`
-    console.log(new_rgba)
     };
   });
 });
@@ -18,6 +17,7 @@ boxes.forEach((box) => {
 colours.forEach((colour) => {
   colour.addEventListener('click', () => {
     currentColour = getBackgroundColour(colour);
+    currentColour = currentColour.replace('rgb(', '').replace(')', '');
     console.log(currentColour);
   });
 });
