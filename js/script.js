b8c70f781252377d7a5f3f93d53c22b81f49ccfc
Jaylon <jaylonwingfield10@gmail.com>
2022-03-29 23:39:02 +0100

    Added colour changing functionality

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const boxes = document.querySelectorAll('.box');
 const colours = document.querySelectorAll('#colours div');
-let currentColour = '236, 240, 241';
+let currentColour = '66, 66, 66';
 const rgbAlpha = /[0-9].[0-9][0-9]/
 // we use the .forEach method to iterate through each button
 boxes.forEach((box) => {
@@ -9,7 +9,7 @@ boxes.forEach((box) => {
     let current_rgba = parseFloat(rgba.match(rgbAlpha));
     if (current_rgba < 1.0) {
     new_rgba = current_rgba + 0.1
-    box.style.backgroundColor = `rgba(0, 0, 0, ${new_rgba})`
+    box.style.backgroundColor = `rgba(${currentColour}, ${new_rgba})`
     };
   });
 });
@@ -18,7 +18,6 @@ colours.forEach((colour) => {
   colour.addEventListener('click', () => {
     currentColour = getBackgroundColour(colour);
     currentColour = currentColour.replace('rgb(', '').replace(')', '');
-    console.log(currentColour);
   });
 });
 
