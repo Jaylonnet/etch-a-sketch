542e50abe79fae1494c9eb8d7ced4fb14be4af20
Jaylon <jaylonwingfield10@gmail.com>
2022-03-30 00:28:53 +0100

    Encapsulated event generator into a function for reuse

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const container = document.querySelector('.container')
 function createGrid(num) {
   if (container.firstElementChild) {
     clearGrid();
-  };
+  }
   for (let i = 0; i < num; i++) {
     const col = document.createElement('div');
     col.classList.add('col');
@@ -14,6 +14,7 @@ function createGrid(num) {
     };
     container.appendChild(col);
   };
+  events();
 };
 
 function clearGrid() {
@@ -24,7 +25,6 @@ function clearGrid() {
 
 createGrid(64);
 
-const boxes = document.querySelectorAll('.box');
 const colours = document.querySelectorAll('#colours div');
 
 //default grey rgb values
@@ -34,16 +34,19 @@ let currentColour = '66, 66, 66';
 const rgbAlpha = /[0-9].[0-9][0-9]/
 
 //foreach to set new rgba values on mouseover
-boxes.forEach((box) => {
-  box.addEventListener('mouseover', () => {
-    let rgba = getBackgroundColour(box);
-    let current_rgba = parseFloat(rgba.match(rgbAlpha));
-    if (current_rgba < 1.0) {
-      new_rgba = current_rgba + 0.2
-      box.style.backgroundColor = `rgba(${currentColour}, ${new_rgba})`
-    };
+function events() {
+  const boxes = document.querySelectorAll('.box');
+  boxes.forEach((box) => {
+    box.addEventListener('mouseover', () => {
+      let rgba = getBackgroundColour(box);
+      let current_rgba = parseFloat(rgba.match(rgbAlpha));
+      if (current_rgba < 1.0) {
+        new_rgba = current_rgba + 0.2
+        box.style.backgroundColor = `rgba(${currentColour}, ${new_rgba})`
+      };
+    });
   });
-});
+};
 
 //event listener to change colour
 colours.forEach((colour) => {
